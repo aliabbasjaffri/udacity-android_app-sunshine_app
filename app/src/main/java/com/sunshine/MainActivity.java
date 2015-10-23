d0876015272216f87b7446b56a9985ba20be84b8
Ali Abbas Jaffri <megahertz888@gmail.com>
2015-10-24 00:15:47 +0500

    Setting up base fragment activity with a fragment

--- app/src/main/java/com/sunshine/MainActivity.java
+++ app/src/main/java/com/sunshine/MainActivity.java
@@ -1,14 +1,42 @@
 package com.sunshine;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if( savedInstanceState == null )
+        {
+            getSupportFragmentManager().beginTransaction().add(R.id.container , new PlaceholderFragment()).commit();
+        }
     }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment
+    {
+        public PlaceholderFragment()
+        {
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            return inflater.inflate(R.layout.fragment_main, container, false);
+            //return rootView;
+        }
+    }
+
 }
