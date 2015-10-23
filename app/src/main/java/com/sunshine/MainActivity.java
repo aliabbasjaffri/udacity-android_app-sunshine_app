69453cb3c4b2269e0ea888412aa5a17590ebef58
Ali Abbas Jaffri <megahertz888@gmail.com>
2015-10-23 23:49:54 +0500

    First Commit

--- app/src/main/java/com/sunshine/MainActivity.java
+++ app/src/main/java/com/sunshine/MainActivity.java
@@ -3,7 +3,8 @@ package com.sunshine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
