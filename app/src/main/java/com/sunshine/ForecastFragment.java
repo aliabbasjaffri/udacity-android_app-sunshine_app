643d7923657863b6e675f4914ac94ed23682afe6
Ali Abbas Jaffri <megahertz888@gmail.com>
2015-10-24 20:18:57 +0500

    Added Menu To Refresh

--- app/src/main/java/com/sunshine/ForecastFragment.java
+++ app/src/main/java/com/sunshine/ForecastFragment.java
@@ -5,16 +5,17 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -31,10 +32,36 @@ public class ForecastFragment extends Fragment
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        inflater.inflate(R.menu.forecastfragment_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh)
+        {
+            new ForecastFetchTask().execute();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-        new ForecastFetchTask().execute();
+        //new ForecastFetchTask().execute();
 
         weatherData.add("Today - Sunny - 88/63");
         weatherData.add("Today - Rainy - 50/63");
@@ -78,7 +105,7 @@ public class ForecastFragment extends Fragment
 
                 reader = new BufferedReader(new InputStreamReader(inputStream));
 
-                String line = "";
+                String line;
 
                 while ((line = reader.readLine()) != null)
                 {
