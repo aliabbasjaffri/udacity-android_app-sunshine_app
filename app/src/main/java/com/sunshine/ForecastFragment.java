4af8d237fd3c5d44e721e09ee08aa4916e3def3b
Ali Abbas Jaffri <megahertz888@gmail.com>
2015-10-24 12:32:00 +0500

    Added separate Fragment Class Made Async Task to get message from the API. Getting message from the API and outputting it on the logcat

---
+++ app/src/main/java/com/sunshine/ForecastFragment.java
@@ -0,0 +1,121 @@
+package com.sunshine;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by aliabbasjaffri on 24/10/15.
+ */
+public class ForecastFragment extends Fragment
+{
+    ArrayList<String> weatherData = new ArrayList<>();
+    String forecastJsonStr = null;
+
+    public ForecastFragment( )
+    {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        new ForecastFetchTask().execute();
+
+        weatherData.add("Today - Sunny - 88/63");
+        weatherData.add("Today - Rainy - 50/63");
+        weatherData.add("Today - Frosty - 09/63");
+
+
+        ListView listView = (ListView) view.findViewById(R.id.listView_forecast);
+        ForecastAdapter forecastAdapter = new ForecastAdapter( getActivity() ,R.layout.list_item_forecast , weatherData);
+        listView.setAdapter(forecastAdapter);
+
+        return view;
+    }
+
+    class ForecastFetchTask extends AsyncTask<Void, Void, Void>
+    {
+        private final String LOG_TAG = ForecastFetchTask.class.getSimpleName();
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            try
+            {
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Lahore,PK&mode=json&units=metric&cnt=7";
+                String apiKey = "&appid=bd82977b86bf27fb59a04b61b657fb6f";
+                String finalUrl = baseUrl + apiKey;
+                URL url = new URL(finalUrl);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if (inputStream == null)
+                    return null;
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line = "";
+
+                while ((line = reader.readLine()) != null)
+                {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0)
+                    return null;
+
+                forecastJsonStr = buffer.toString();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                Log.e("data", forecastJsonStr);
+
+                if (urlConnection != null)
+                {
+                    urlConnection.disconnect();
+                }
+                if (reader != null)
+                {
+                    try
+                    {
+                        reader.close();
+                    }
+                    catch (final IOException e)
+                    {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- app/src/main/java/com/sunshine/MainActivity.java
+++ app/src/main/java/com/sunshine/MainActivity.java
@@ -3,55 +3,32 @@ package com.sunshine;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
-    static ArrayList<String> weatherData = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        weatherData.add("Today - Sunny - 88/63");
-        weatherData.add("Today - Rainy - 50/63");
-        weatherData.add("Today - Frosty - -09/63");
-
-        if( savedInstanceState == null )
+        if (savedInstanceState == null)
         {
-            getSupportFragmentManager().beginTransaction().add(R.id.container , new PlaceholderFragment()).commit();
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment
-    {
-        ArrayList<String> data = new ArrayList<>();
-
-        public PlaceholderFragment( )
-        {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            View view = inflater.inflate(R.layout.fragment_main, container, false);
-
-            ListView listView = (ListView) view.findViewById(R.id.listView_forecast);
-            ForecastAdapter forecastAdapter = new ForecastAdapter( getActivity() ,R.layout.list_item_forecast , weatherData);
-            listView.setAdapter(forecastAdapter);
-
-            return view;
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new ForecastFragment()).commit();
         }
     }
 
