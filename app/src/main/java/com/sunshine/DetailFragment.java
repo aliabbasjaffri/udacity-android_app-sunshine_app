6d865efabddcefeb0a7cbd9a41e29b175fe33f9a
Ali Abbas Jaffri <megahertz888@gmail.com>
2015-11-01 15:25:36 +0500

    Added Sharing Options!

--- app/src/main/java/com/sunshine/DetailFragment.java
+++ app/src/main/java/com/sunshine/DetailFragment.java
@@ -3,6 +3,9 @@ package com.sunshine;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,7 +19,9 @@ import android.widget.TextView;
  */
 public class DetailFragment extends Fragment {
 
-    String weatherReport;
+    private String weatherReport;
+    private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
     public DetailFragment()
     {
@@ -34,6 +39,28 @@ public class DetailFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
     {
         inflater.inflate(R.menu.menu_detail, menu);
+
+        MenuItem item = menu.findItem(R.id.action_share);
+
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        if (mShareActionProvider != null )
+        {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }
+        else
+        {
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
+    }
+
+    private Intent createShareForecastIntent()
+    {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, weatherReport + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 
     @Override
